62865fbcdcde8ad3224ee1028a895eba8a291cd9
rickyvelis <37187134+rickyvelis@users.noreply.github.com>
2018-09-28 14:29:48 +0200

    hallo

--- HotelProject/Program.cs
+++ HotelProject/Program.cs
@@ -17,6 +17,7 @@ namespace HotelProject
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //hoi
         }
     }
 }
