560ca1c9071545151dc9856420ea0bf9e9fd227f
Ricky <ricky_wow123@hotmail.com>
2018-09-29 16:16:35 +0200

    JSONconverter added

---
+++ HotelProject/Hotel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace HotelProject
+{
+
+    class Hotel
+    {
+        public Form1 Form { get; set; }
+
+        public Hotel(Form1 Form)
+        {
+            List<Unit> units = JSONreader();
+        }
+
+        public List<Unit> JSONreader()
+        {
+            List<Unit> units;
+
+            try
+            {
+                using(StreamReader r = new StreamReader("Hotel.layout"))
+                {
+                    string json = r.ReadToEnd();
+                    units = JsonConvert.DeserializeObject<List<Unit>>(json);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+            return units;
+        }
+    }
+}
