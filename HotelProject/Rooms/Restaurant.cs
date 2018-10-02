4bff908ca901dec633a5c87b236993c22892a3e9
Jeroen van der Kolk <14064723@student.hhs.nl>
2018-10-02 12:04:15 +0200

    Made a room factory. made base classes for the rooms

---
+++ HotelProject/Rooms/Restaurant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.Rooms
+{
+    class Restaurant : IRoom
+    {
+        public string AreaType { get; set; }
+
+        public Restaurant()
+        {
+            AreaType = "Restaurant";
+        }
+
+    }
+}
---
+++ HotelProject/Rooms/Room.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.Rooms
+{
+    class Room : IRoom
+    {
+        public string AreaType { get; set; }
+
+        public Room()
+        {
+            AreaType = "Room";
+        }
+    }
+}
