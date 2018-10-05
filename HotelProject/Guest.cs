193c54ec67e2e283895592b707dd0065124433d3
Jeroen van der Kolk <14064723@student.hhs.nl>
2018-10-05 14:12:18 +0200

    guests aangemaakt

---
+++ HotelProject/Guest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelProject.Rooms;
+
+namespace HotelProject
+{
+    public class Guest
+    {
+        List<IRoom> Path { get; set; }
+        string Name { get; set; }
+        IRoom Room { get; set; }
+        Point Position { get; set; }
+        private Image Img { get; set; }
+
+    }
+}
