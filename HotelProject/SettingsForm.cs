6d1d7ef673e2835cb824ff87f455d9fdf3a34a5c
rickyvelis <37187134+rickyvelis@users.noreply.github.com>
2018-10-12 13:32:43 +0200

    Begin gemaakt met Settings

---
+++ HotelProject/SettingsForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HotelProject
+{
+    public partial class SettingsForm : Form
+    {
+        private Hotel _Hotel { get; set; }
+
+        public SettingsForm()
+        {
+            //InitializeComponent();
+
+            _Hotel = Hotel.GetInstance();
+        }
+
+        private void start_button_Click(object sender, EventArgs e)
+        {
+            if (cleanerAmount_textBox.Text != "" 
+                //&& cleanerAmount_textBox.Text != ""
+                //&& maxLiftPassengers_textBox.Text != ""
+                //&& sPerHTE_textBox.Text != ""
+                )
+            {
+
+            }
+        }
+    }
+}
